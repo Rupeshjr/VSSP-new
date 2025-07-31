7319f2e66319fdd1c1077d04a7e0822d4ba9a722
Rupesh <115726234+Rupeshjr@users.noreply.github.com>
2025-07-31 10:52:30 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -273,7 +273,7 @@ app = FastAPI(
 # Add CORS middleware
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["harmonious-starburst-f1652c.netlify.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
